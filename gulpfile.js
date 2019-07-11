039cb2afd24d63efbe58d5b1e15e932037692449
barspredator <boris.dryabin@gmail.com>
2019-07-11 14:11:06 +0300

    add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require ('gulp'),
     browserSync = require ('browser-sync');
 
 gulp.task('sass', function(){
-    return gulp.src('app/sass/*.sass')
+    return gulp.src('app/css/*.scss')
         .pipe(sass())
         .pipe(autoprefixer(['last 15 versions', '>1%', 'ie 9'], {cascade: true}))
         .pipe(gulp.dest('app/css'))
@@ -26,7 +26,7 @@ gulp.task('browser-sync', function(){
 });
 
 gulp.task('watch', function(){
-    gulp.watch('app/sass/*.sass', gulp.parallel('sass'));
+    gulp.watch('app/css/*.scss', gulp.parallel('sass'));
     gulp.watch('app/*.html', gulp.parallel('code'));
 });
 
