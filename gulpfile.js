105e61bbdfa653699ff5cba74f7523f220e811a8
barspredator <boris.dryabin@gmail.com>
2019-07-11 17:51:22 +0300

    Иконки другого цвета

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require ('gulp'),
     browserSync = require ('browser-sync');
 
 gulp.task('sass', function(){
-    return gulp.src('app/css/*.scss')
+    return gulp.src('app/css/blocks/*.scss')
         .pipe(sass())
         .pipe(autoprefixer(['last 15 versions', '>1%', 'ie 9'], {cascade: true}))
         .pipe(gulp.dest('app/css'))
@@ -26,7 +26,7 @@ gulp.task('browser-sync', function(){
 });
 
 gulp.task('watch', function(){
-    gulp.watch('app/css/*.scss', gulp.parallel('sass'));
+    gulp.watch('app/css/blocks/*.scss', gulp.parallel('sass'));
     gulp.watch('app/*.html', gulp.parallel('code'));
 });
 
